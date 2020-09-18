83710f9168d76c9d86f5561bb5adb5f3423ad59a
Pranay Prajapati <60659922+pranay-prajapati@users.noreply.github.com>
2020-09-18 10:41:17 +0530

    WIP:Update method

--- Album/AlbumAPI/serializers.py
+++ Album/AlbumAPI/serializers.py
@@ -3,11 +3,14 @@ from .models import Album, Song
 
 
 class SongSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Song
         fields = [
-            'title', 'duration', 'rating', 'genre'
+            'id', 'title', 'duration', 'rating', 'genre'
         ]
+        read_only_fields = ('title',)
 
 
 class AlbumSerializer(serializers.ModelSerializer):
@@ -16,7 +19,7 @@ class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
         fields = [
-            'title', 'description', 'year', 'rating', 'songs',
+            'id', 'title', 'description', 'year', 'rating', 'songs'
         ]
 
     def create(self, validated_data):
@@ -25,3 +28,28 @@ class AlbumSerializer(serializers.ModelSerializer):
         for song_data in songs_data:
             Song.objects.create(album=album, **song_data)
         return album
+
+    def update(self, instance, validated_data):
+        songs_data = validated_data.pop('songs')
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        keep_songs = []
+        for song_data in songs_data:
+            if "id" in song_data.keys():
+                if Song.objects.filter(id=song_data['id']).exists():
+                    s = Song.objects.get(id=song_data['id'])
+                    s.title = song_data.get('title', s.title)
+                    s.save()
+                    keep_songs.append(s.id)
+                else:
+                    continue
+            else:
+                s = Song.objects.create(**song_data, album=instance)
+                keep_songs.append(s.id)
+
+        for song_data in instance.songs_data:
+            if song_data.id not in keep_songs:
+                song_data.delete()
+
+        return instance
